44eb531d9315427a3b7ef3e1acd0b54d7af071e8
stasianich <spechenuy@gmail.com>
2021-09-16 18:11:30 +0300

    2 files modified, 1 added

---
+++ src/app/cart.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+
+@Injectable()
+export class CartService {
+  cart: Product[] = [];
+
+  constructor() { }
+
+  addToCart(product: Product) {
+    this.cart.push(product);
+  }
+
+  getCart() {
+    return this.cart;
+  }
+
+  clearCart() {
+    this.cart = [];
+
+    return this.cart;
+  }
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 import { Product, products } from '../products';
 
 @Component({
@@ -10,7 +11,10 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
     // First get the product id from the current route.
@@ -21,4 +25,8 @@ export class ProductDetailsComponent implements OnInit {
     this.product = products.find(product => product.id === productIdFromRoute);
   }
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert(`${product.name} has been added to the cart!`);
+  }
 }
